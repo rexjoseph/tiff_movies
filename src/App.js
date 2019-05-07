f49887412291528d1901c1c936db05a9d9c9224c
rexjoseph <josephrex96@gmail.com>
2019-05-07 18:25:06 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './App.css';
+import MovieRow from './MovieRow.js';
+import $ from 'jquery';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+  this.performSearch('Gandhi')
+}
+
+  performSearch(searchTerm) {
+    console.log('Perform something')
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=0707c7b36e972720fd24c63c4f2e45c9&query=" + searchTerm;
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        const results = searchResults.results;
+
+       var movieRows = []
+       results.forEach((movie) => {
+        movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+         const movieRow = <MovieRow key={movie.id} movie={movie} />
+         movieRows.push(movieRow)
+       })
+
+       this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.log('Error');
+      }
+    })
+  }
+
+  searchChangeHandler(e) {
+    const boundObject = this;
+    const searchTerm = e.target.value
+    boundObject.performSearch(searchTerm);
+  }
+
+  render() {
+    return(
+      <header className="nav_header">
+       <nav className="navbar">
+         <div className="container">
+         <ul className="flex">
+           <li id="logo"><a href="https://www.tiff.net/">tiff</a></li>
+           <li><a href="#">Festivals</a></li>
+           <li><a href="#">Year-Round</a></li>
+           <li><a href="#">Join + Give</a></li>
+           <li><a href="#">Visits</a></li>
+           <li><a href="#">The Review</a></li>
+           <li><a href="#">Shop</a></li>
+         </ul>
+         </div>
+       </nav>
+       <article class="container">
+       <form>
+       <input id="search" type="search" onChange={this.searchChangeHandler.bind(this)} placeholder="Search Term" />
+       </form>
+          {this.state.rows}
+       </article>
+     </header>
+    )
+  }
+}
+  
+
+export default App;
---
+++ src/MovieRow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class MovieRow extends React.Component {
+  viewMovie() {
+    const urlRedirect = 'https://www.themoviedb.org/movie/' + this.props.movie.id;
+    window.location.href = urlRedirect;
+  }
+    render() {
+        return (
+        <div className="grid" key={this.props.movie.id}>
+          <div className="center">
+              <img src={this.props.movie.poster_src} alt="banner" />
+              <h1>{this.props.movie.title}</h1>
+              <p>{this.props.movie.overview}</p>
+              <p id="popularity">Popularity: {this.props.movie.popularity}</p>
+              <p id="release">Release Date: {this.props.movie.release_date}</p>
+              <input id="btn" type="button" onClick={this.viewMovie.bind(this)} value="View Info" />
+            </div>
+        </div>
+        )
+    }
+}
+
+export default MovieRow;
